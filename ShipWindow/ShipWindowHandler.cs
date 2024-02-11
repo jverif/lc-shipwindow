c6a65626bae20cfbaf5a1f1a728d8d3fc90d2a09
veri <verifiableyt@gmail.com>
2024-02-10 18:53:04 -0600

    Initial commit for upload

---
+++ ShipWindow/ShipWindowHandler.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace ShipWindow
+{
+    public class ShipWindowHandler : NetworkBehaviour
+    {
+
+        private static ShipWindowHandler _instance;
+        public static ShipWindowHandler Instance {
+            get
+            {
+                if (_instance == null)
+                    _instance = UnityEngine.Object.FindObjectOfType<ShipWindowHandler>();
+                if (_instance == null)
+                    ShipWindowPlugin.mls.LogWarning("ShipWindowHandler Could Not Be Found! Returning Null!");
+                return _instance;
+            }
+            set { _instance = value; }
+        }
+        public Animator windowAnimator;
+        public GameObject universeVolume;
+        public GameObject starSphereLarge;
+
+        public override void OnNetworkSpawn()
+        {
+
+            ShipWindowPlugin.mls.LogInfo("Network window spawn");
+
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+
+            GameObject windowShipObject = ShipWindowPlugin.newShipInstance;
+            if (windowShipObject != null)
+                windowAnimator = windowShipObject.transform.Find("WindowContainer/Window").GetComponent<Animator>();
+
+            universeVolume = ShipWindowPlugin.universeVolume;
+            starSphereLarge = ShipWindowPlugin.starSphereLarge;
+
+            SetWindowState(false);
+
+            base.OnNetworkSpawn();
+        }
+
+        [ClientRpc]
+        public void SetWindowStateClientRpc(bool closed)
+        {
+            SetWindowState(closed);
+        }
+
+        public void SetWindowState(bool closed)
+        {
+            if (ShipWindowPlugin.enableShutter.Value == true)
+                windowAnimator?.SetBool("Closed", closed);
+        }
+
+        [ClientRpc]
+        public void SetVolumeStateClientRpc(bool enabled)
+        {
+            SetVolumeState(enabled);
+        }
+
+        public void SetVolumeState(bool enabled)
+        {
+            switch (ShipWindowPlugin.spaceOutsideSetting.Value)
+            {
+                case 0: break;
+                case 1: universeVolume?.SetActive(enabled); break;
+                case 2: starSphereLarge?.SetActive(enabled); break;
+                default: break;
+            }
+        }
+    }
+}
---
+++ out/README.md
@@ -0,0 +1,39 @@
+# Ship Window
+
+Adds a glass window to the drop ship so you can see outside. The Company didn't see it as important, so they put the terminal computer in front of it. Be sure to move that!
+
+All players must install this mod or there may be desync issues.
+
+## Planned
+
+- See orbited moon from space
+
+## Update History
+
+- **1.1.0**
+    - Add config settings for controlling window shutter, outside space props, and outside space volume.
+
+- **1.0.7**
+    - Fix network object being destroyed for some reason. (?)
+
+- **1.0.6**
+    - Re-add window shutter and outside view in space.
+
+- **1.0.5**
+    - Disable window shutter and stars (big oof). Will add back soon.
+
+- **1.0.4**
+    - Add a shutter to the window while loading.
+    - Fix stars being visible at Company Building.
+
+- **1.0.3**
+    - Fix stars being always visible and enemies being invisible through glass.
+
+- **1.0.2**
+    - Update description.
+
+- **1.0.1**
+    - Added stars outside the window while in space.
+
+- **1.0.0**
+    - Initial release.
